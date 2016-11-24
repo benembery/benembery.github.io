5b0d934eecc9fef2abd748a2ca840a62a5275ee0
Ben Embery <benembery@Bens-MacBook-Pro.local>
2016-11-24 11:00:06 +0000

    Include npm packages required for LESS to CSS compilation Include stock LESS compilation gulp file.

---
+++ gulpfile.js
@@ -0,0 +1,81 @@
+/// <binding BeforeBuild='default' />
+/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+'use strict'
+
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    rename = require('gulp-rename'),
+    del = require('del'),
+    // LESS dependendcies
+    less = require('gulp-less'),
+    postcss = require('gulp-postcss'),
+    autoprefix = require('autoprefixer'),
+    cssnano = require('cssnano'),
+    cssmqp = require('css-mqpacker'),
+    sourcemaps = require('gulp-sourcemaps')
+
+var config = {
+    src_less: './assets_src/less/*.less',
+    css_output_path: './assets/css'
+}
+
+gulp.task('default', ['bundle:css'])
+
+gulp.task('clean:css', function() {
+    return del(config.css_output_path+'/*.*')
+});
+
+gulp.task('bundle:css', ['clean:css'], function () {
+    return gulp.src([config.src_less])
+            .on('error', mapError)
+            .pipe(sourcemaps.init())
+            .pipe(less())
+            .pipe(postcss([autoprefix({ browsers: ['last 2 versions'] }), cssmqp({ sort: true }), cssnano]))
+            .pipe(rename({ suffix: '.min' }))
+            .pipe(sourcemaps.write('./', { sourceRoot: '../../assets_src/less/' }))
+            .pipe(gulp.dest(config.css_output_path))
+})
+
+gulp.task('set-dev', function() {
+    process.env.NODE_ENV = 'development'
+})
+
+
+gulp.task('dev', function () {
+    gulp.watch(config.src_less, ['bundle:css'])
+})
+
+
+function isProduction() {
+    return process.env.NODE_ENV === 'production'
+}
+
+function mapError(err) {
+    var msg = ''
+
+    if (err.type) {
+        msg = '\n' + gutil.colors.red(err.type + ' ' + err.name + ': ')
+
+        if (err.message) {
+            msg += gutil.colors.red(err.message + '\n')
+        }
+
+        msg += gutil.colors.white(err.filename.split('assets\\')[1]) +
+            gutil.colors.gray(' line: ', err.lineNumber, ' col: ', (err.columnNumber || err.column), '\n') +
+            gutil.colors.cyan(err.extract.join(gutil.colors.magenta('\n')) + '\n')
+
+    }
+    else {
+        msg = gutil.colors.red('Error: ' + err.message) + '\n' + gutil.colors.gray(err)
+    }
+
+    gutil.log(msg)
+
+    if (isProduction())
+        process.exit(1)
+
+    this.emit('end')
+}
